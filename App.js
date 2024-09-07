35853c6dd9f75df2306f4d8b3f97502d51dfad37
SNext <snext2004@gmail.com>
2024-09-07 18:17:31 +0700

    add: Physics.js สร้างฟิสิกส์แรงดึงดูด

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import entities from './entities';
+import Physics from './Physics'
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      <GameEngine 
+      <GameEngine
+      systems={[Physics]}
       entities={entities()}
       style={{position: 'absolute',top: 0,left: 0,right: 0,bottom: 0}}
       >
