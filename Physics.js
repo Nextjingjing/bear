c80b1f82ae5c7479006ed7d02d2b4f58cb63be92
SNext <snext2004@gmail.com>
2024-09-09 20:03:22 +0700

    Physics: รับ input move

--- Physics.js
+++ Physics.js
@@ -1,11 +1,28 @@
-import Matter from "matter-js"
+import Matter from "matter-js";
 
-const Physics = (entities, {touches, time, dispatch}) => {
-    let engine = entities.physics.engine
+const Physics = (entities, { touches, time, dispatch }) => {
+  let engine = entities.physics.engine;
 
-    Matter.Engine.update(engine, time.delta)
+  // Find the first 'move' touch event, if there is one
+  let move = touches.find(t => t.type === 'move');
 
-    return entities
-}
+  // If a 'move' touch event exists and the Bear entity is present
+  if (move && entities.Bear) {
+    // Calculate the velocity based on touch movement (deltaX, deltaY)
+    const velocityX = move.delta.pageX;
+    const velocityY = move.delta.pageY;
+
+    // Set velocity for the Bear's body using Matter.Body.setVelocity
+    Matter.Body.setVelocity(entities.Bear.body, {
+      x: velocityX,
+      y: velocityY,
+    });
+  }
+
+  // Update the engine with the current time step
+  Matter.Engine.update(engine, time.delta);
+
+  return entities;
+};
 
 export default Physics;
